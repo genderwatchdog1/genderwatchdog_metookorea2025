f2c13e1b73291dce5964a6aca69eadcf82c11167
Gender Watchdog <genderwatchdog@gmail.com>
2025-05-28 07:13:37 -0400

    Refactor randomize_git_dates.sh to improve commit date randomization logic. The script now generates dates starting from 30 days ago, with random offsets for each commit, ensuring a more varied and realistic timeline. This update enhances the project's version control practices while maintaining focus on advocacy against sexual violence at Dongguk University.

--- randomize_git_dates.sh
+++ randomize_git_dates.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # Get all commit hashes in reverse (oldest first)
-commits=$(git rev-list --reverse HEAD)
+commits=($(git rev-list --reverse HEAD))
 
-for commit in $commits; do
-    # Generate a random date in the last 60 days, between 9:00 and 18:59
-    rand_date=$(python3 -c "
-import random, datetime
-days_ago = random.randint(0, 59)
-hour = random.randint(9, 18)
-minute = random.randint(0, 59)
-second = random.randint(0, 59)
-dt = datetime.datetime.now() - datetime.timedelta(days=days_ago)
-dt = dt.replace(hour=hour, minute=minute, second=second, microsecond=0)
-print(dt.strftime('%Y-%m-%dT%H:%M:%S-04:00'))
-")
-    # Rewrite the commit date
-    GIT_COMMITTER_DATE="$rand_date" GIT_AUTHOR_DATE="$rand_date" git filter-branch -f --env-filter "
-        if [ \$GIT_COMMIT = $commit ]; then
-            export GIT_AUTHOR_DATE='$rand_date'
-            export GIT_COMMITTER_DATE='$rand_date'
+# Start from 30 days ago, incrementing by a random amount for each commit
+base_date=$(date -d "30 days ago" +%s)
+
+for i in "${!commits[@]}"; do
+    # Add a random offset (0-2 days) to the base date for each commit
+    offset_days=$((RANDOM % 3))
+    commit_date=$((base_date + (i + offset_days) * 86400))
+    # Random hour/minute/second between 9:00 and 18:59
+    hour=$((RANDOM % 10 + 9))
+    minute=$((RANDOM % 60))
+    second=$((RANDOM % 60))
+    # Format as NY time (EDT, -04:00)
+    formatted_date=$(date -d "@$commit_date" +"%Y-%m-%dT%02d:%02d:%02d-04:00" $hour $minute $second)
+    export GIT_COMMITTER_DATE="$formatted_date"
+    export GIT_AUTHOR_DATE="$formatted_date"
+    # Rewrite this commit
+    git filter-branch -f --env-filter "
+        if [ \$GIT_COMMIT = ${commits[$i]} ]; then
+            export GIT_AUTHOR_DATE='$formatted_date'
+            export GIT_COMMITTER_DATE='$formatted_date'
         fi
-    " $commit^..$commit
+    " ${commits[$i]}^..${commits[$i]}
 done
